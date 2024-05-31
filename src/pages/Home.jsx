76430d771eab30e983102689763a1ff31cc705e9
Rahu1Singh <srahul1357@gmail.com>
2024-05-31 11:20:28 +0530

    made pages for the website with routes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Hero from "../components/Hero"
+import Brands from "../components/Brands"
+import Description from "../components/Description"
+import Footer from "../components/Footer"
+
+const Home = () => {
+  return (
+    <div className="">
+      <Hero />
+      <Brands />
+      <Description />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
