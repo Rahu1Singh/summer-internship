d95b6ef515fbe0f0b7407c229b317ccfe929b7c0
Rahu1Singh <srahul1357@gmail.com>
2024-05-31 16:28:45 +0530

    made the components responsive

--- src/components/Domains.jsx
+++ src/components/Domains.jsx
@@ -4,14 +4,14 @@ import Domainsweserve from "./Domainsweserve";
 const Domains = () => {
     return (
         <div id="domains" className="relative w-full bg-b-100 overflow-hidden flex flex-col items-center">
-            <h1 className="z-10 text-4xl font-bold text-b-800 my-5">Who We Are</h1>
+            <h1 className="z-10 text-4xl font-bold text-b-800 mt-8">Who We Are</h1>
             <div id="whoweare" className="my-10 relative p-5 flex justify-center items-center w-full max-w-4xl">
                 <Whoweare />
-                <div className="absolute top-[50px] left-[50px] z-10 h-[666px] w-[747px] rounded-2xl bg-b-800" />
+                <div className="hidden md:block absolute top-[50px] left-[50px] z-10 h-[666px] w-[747px] rounded-2xl bg-b-800" />
             </div>
             
-            <div className="my-10 relative p-5 flex justify-center items-center w-full">
-                <h1 className="z-10 text-[50px] leading-[56px] text-right w-1/3 font-bold text-b-800 my-5 px-20">Domains<br />We Serve</h1>
+            <div className="my-10 relative p-5 flex max-lg:flex-col justify-center items-center w-full">
+                <h1 className="z-10 text-[50px] leading-[56px] text-center lg:text-right lg:w-1/3 font-bold text-b-800 my-2 lg:my-5 lg:px-20">Domains<br />We Serve</h1>
                 <Domainsweserve />
             </div>
             <div className="absolute -left-[1050px] top-[650px] h-[700px] w-[3270px] -rotate-45 z-0 bg-b-300"></div>
--- src/components/Whoweare.jsx
+++ src/components/Whoweare.jsx
@@ -2,12 +2,12 @@ import { whoweareitems } from "../constants"
 
 const Whoweare = () => {
   return (
-    <div className="z-20 h-[666px] w-[747px] rounded-2xl bg-[#EFEFEF] flex flex-wrap justify-around align-center p-5">  
+    <div className="z-20 h-[666px] w-[747px] rounded-2xl bg-b-200 flex flex-wrap md:grid md:grid-cols-3 p-2 md:p-5">  
         {whoweareitems.map((item) => (
-            <div className="h-[265px] w-[200px] flex flex-col items-center p-2 my-5">
+            <div key={item.id} className="md:h-[265px] md:w-[200px] flex md:flex-col items-center p-2 md:my-5">
                 <img src={item.icon} className="h-[50px] w-[50px] m-3"></img>
-                <h1 className="font-bold text-[17px] text-center py-2 ">{item.title}</h1>
-                <p>{item.text}</p>
+                <h1 className="font-bold text-[17px] text-start md:text-center py-2 ">{item.title}</h1>
+                <p className="hidden md:block">{item.text}</p>
             </div>
         ))}
     </div>
