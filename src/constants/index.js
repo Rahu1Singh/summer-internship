f54bc7ca6f848ed680af5779ad9fc129e9886b02
Rahu1Singh <srahul1357@gmail.com>
2024-05-31 15:39:18 +0530

    added domains content

--- src/constants/index.js
+++ src/constants/index.js
@@ -73,3 +73,79 @@ export const herotext = [
         text: "Pioneering Innovation: Crafting Complete and Effective Products for Your Business Needs."
     }
 ]
+
+//Who we are Component
+import {professional,experience,certified,iso,location,hikvision} from "../assets"
+export const whoweareitems = [
+    {
+        id: "0",
+        title: "Experienced Professionals",
+        text: "Started by experienced automation professionals in the automation industry, to provide innovative solutions in Automation, IT and Engineering",
+        icon: professional,
+    },
+    {
+        id: "1",
+        title: "21 Years Of Experience",
+        text: "Over 21 years of Project Design, Engineering, Programming, Testing & commissioning experience in Process Automation, Building Automation & Software Development",
+        icon: experience,
+    },
+    {
+        id: "2",
+        title: "BizSAFE & EcoXpert Certified",
+        text: " Ingenium Automation + Engineering a BizSAFE star certified Company & EcoXpert Certified partner with Schneider Electric in Singapore",
+        icon: certified,
+    },
+    {
+        id: "3",
+        title: "ISO Certified",
+        text: "Ingenium Singapore is ISO 9001:2015 , ISO 45001 : 2018 & ISO 14001 : 2015 Certified Company",
+        icon: iso,
+    },
+    {
+        id: "4",
+        title: "Ingenium @ India",
+        text: "Ingenium\'s origin extended to India in 2017 as Ingenium Automation India Private Limited an EcoXpert certified partner to Schneider Electric India",
+        icon: location,
+    },
+    {
+        id: "5",
+        title: "Authorized HikVision System Integrator",
+        text: "Ingenium Automation is an authorized System Integrator of Hikvision in CCTV & Access Control System.",
+        icon: hikvision,
+    },
+]
+
+//Domains component
+import {healthcare,airports,datacenter,lifescience,hotels,commercialestates} from "../assets"
+export const domainitems = [
+    {
+        id: "0",
+        text: "Healthcare",
+        icon: healthcare,
+    },
+    {
+        id: "1",
+        text: "Airports",
+        icon: airports,
+    },
+    {
+        id: "2",
+        text: "Data Center",
+        icon: datacenter,
+    },
+    {
+        id: "3",
+        text: "Life Science",
+        icon: lifescience,
+    },
+    {
+        id: "4",
+        text: "Hotels",
+        icon: hotels,
+    },
+    {
+        id: "5",
+        text: "Commercial Estates",
+        icon: commercialestates,
+    },
+]
